9905157f0daa1f5623dfbe8dca3cd818ca288004
Mauricio Maniglia <manigliamauricio@gmail.com>
2022-03-16 08:00:07 -0300

    Use explicit JSON data for non 200 http responses.

--- Landmark-swift/RemoteLandmarkLoader.swift
+++ Landmark-swift/RemoteLandmarkLoader.swift
@@ -38,8 +38,8 @@ public class RemoteLandmarkLoader {
     public func load(completion: @escaping (Result) -> Void) {
         httpClient.get(fromURL: url) { result in
             switch result {
-            case let .success(data, _):
-                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+            case let .success(data, response):
+                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
                     completion(.success(root.items))
                 } else {
                     completion(.failure(.invalidData))
--- Landmark-swiftTests/RemoteLandmarkLoaderTests.swift
+++ Landmark-swiftTests/RemoteLandmarkLoaderTests.swift
@@ -51,7 +51,8 @@ class RemoteLandmarkLoaderTests: XCTestCase {
 
         samples.enumerated().forEach { index, code in
             expect(sut, toCompleteWithResult: .failure(.invalidData), when: {
-                client.complete(withStatusCode: code, at: index)
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data: json, at: index)
             })
         }
     }
@@ -116,7 +117,7 @@ class RemoteLandmarkLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
 
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
             let response = HTTPURLResponse(url: requestURLs[index],
                                            statusCode: code,
                                            httpVersion: nil,
