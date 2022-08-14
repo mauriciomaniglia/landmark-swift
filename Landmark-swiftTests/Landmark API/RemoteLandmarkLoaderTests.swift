b324a5f0b3327d47ccd2bd1eac8234d78d582ba9
mauriciomaniglia <manigliamauricio@gmail.com>
2022-08-14 17:47:46 -0300

    Extract memory leak tracking helper method to a shared extension on XCTestCase.

--- Landmark-swiftTests/Landmark API/RemoteLandmarkLoaderTests.swift
+++ Landmark-swiftTests/Landmark API/RemoteLandmarkLoaderTests.swift
@@ -119,12 +119,6 @@ class RemoteLandmarkLoaderTests: XCTestCase {
 
         return (sut, httpClient)
     }
-    
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
 
     private class HTTPClientSpy: HTTPClient {
         var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
